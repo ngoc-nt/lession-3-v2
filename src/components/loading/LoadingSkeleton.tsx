cdf6a0c0f9d419b3e1d700cf75f84566ead81995
ngocnt-jvb <nguyen.ngoc@jvb-corp.com>
2023-12-28 08:59:59 +0700

    fix layout skeleton

--- src/components/loading/LoadingSkeleton.tsx
+++ src/components/loading/LoadingSkeleton.tsx
@@ -1,22 +1,20 @@
 import React from "react";
+import styles from "./LoadingSkeleton.module.scss";
 
-interface LoadingSkeletonProps {
-    height: string | number;
-    width?: string | number;
-    radius?: string | number;
-}
-
-const LoadingSkeleton: React.FC<LoadingSkeletonProps> = (props) => {
+const LoadingSkeleton: React.FC = () => {
     return (
-        <div
-            className="skeleton bg-white p-3 rounded-2xl shadow-sm d-flex flex-column"
-            style={{
-                height: props.height,
-                width: props.width || "100%",
-                borderRadius: props.radius,
-            }}
-        >
-            {" "}
+        <div>
+            <div className={`${styles.isloading}`} style={{ height: '100%'}}>
+                <div className={`${styles.loadingImage}`} style={{ height: '500px'}}></div>
+                <div className={`d-xl-flex justify-content-between align-items-center ${styles.textContent}`}>
+                    <div className={`${styles.textContainer}`}>
+                        <div className={`${styles.mainText}`}></div>
+                        <div className={`${styles.subText}`}></div>
+                    </div>
+                    <div className={`${styles.loadingBtn}`}></div>
+                    <div className={`${styles.loadingBtn}`}></div>
+                </div>
+            </div>
         </div>
     );
 };
